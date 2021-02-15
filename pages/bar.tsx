01fc98fa8d21e83d372034002d295f350fed23d6
hisomura <hisomura.noreply@gmail.com>
2021-02-15 13:40:41 +0900

    ページ作ってリンク貼って動作確認。
    .nextを見るとbar.htmlとfoo.htmlが無いのでSSGではなくてSSRで動いてるように見える。
    
    reduxのdev-toolsが有効になってなくてchromeで状態を確認できなかった。

--- pages/bar.tsx
+++ pages/bar.tsx
@@ -3,11 +3,11 @@ import { State } from "../store/store";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
-export default function Home() {
+export default function Bar() {
   const tick = useSelector<State>((state) => state.tick);
   return (
     <div className={styles.container}>
-      <h1>Home</h1>
+      <h1>Bar</h1>
       <div>tick: {tick}</div>
       <Link href="/">Home</Link>
       <Link href="/foo">Foo</Link>
--- pages/foo.tsx
+++ pages/foo.tsx
@@ -3,11 +3,11 @@ import { State } from "../store/store";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
-export default function Home() {
+export default function Foo() {
   const tick = useSelector<State>((state) => state.tick);
   return (
     <div className={styles.container}>
-      <h1>Home</h1>
+      <h1>Foo</h1>
       <div>tick: {tick}</div>
       <Link href="/">Home</Link>
       <Link href="/foo">Foo</Link>
