572cf3d26d8de80d16f69381068a17f4cd9f8f44
hisomura <hisomura.noreply@gmail.com>
2021-02-19 07:11:47 +0900

    Reduxのstateを更新する処理を追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,31 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { State } from "../store/store";
 import styles from "../styles/Home.module.css";
 import Links from "./links";
 
 export default function Home() {
+  const dispatch = useDispatch();
   const tick = useSelector<State>((state) => state.tick);
+
   return (
     <div className={styles.container}>
       <h1>Home</h1>
       <div>tick: {tick}</div>
       <Links />
+      <button
+        onClick={() => {
+          dispatch({ type: "TICK", payload: "Tick 1 clicked." });
+        }}
+      >
+        Tick 1
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "TICK", payload: "Tick 2 clicked." });
+        }}
+      >
+        Tick 2
+      </button>
     </div>
   );
 }
