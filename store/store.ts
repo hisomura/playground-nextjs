c091b24680e93d775800f3a6f89327d4830475b9
hisomura <hisomura.noreply@gmail.com>
2021-02-18 18:07:18 +0900

    サーバーサイドでwindowが存在しなくてenhancerにfalseが入力されるためだったっぽい。
    ブラウザ側で表示されるときにaction.type === HYDRATEが実行される気がしたのだけどそういう動きをしてない。

--- store/store.ts
+++ store/store.ts
@@ -1,4 +1,4 @@
-import { createStore, AnyAction } from "redux";
+import { createStore, AnyAction, compose } from "redux";
 import { MakeStore, createWrapper, Context, HYDRATE } from "next-redux-wrapper";
 
 export interface State {
@@ -24,14 +24,19 @@ const reducer = (state: State = { tick: "init" }, action: AnyAction) => {
   }
 };
 
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    (window as any).window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__()) ||
+  compose;
+
+// https://github.com/zalmoxisus/redux-devtools-extension#13-use-redux-devtools-extension-package-from-npm
+//  サンプル見る限りこれで動きそうだったけどfunctionじゃなくて動かないって言われてしまった
+//  (typeof window !== 'undefined' && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
 // create a makeStore function
 const makeStore: MakeStore<State> = (_context: Context) =>
-  createStore(
-    reducer,
-    typeof window !== "undefined" &&
-      (window as any)?.__REDUX_DEVTOOLS_EXTENSION__ &&
-      (window as any)?.__REDUX_DEVTOOLS_EXTENSION__()
-  );
+  createStore(reducer, composeEnhancers);
 
 // export an assembled wrapper
 export const wrapper = createWrapper<State>(makeStore, { debug: true });
